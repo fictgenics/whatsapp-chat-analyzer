86a6e72c86c0f8a74918cedd57bad77923206f66
Lakshya Jain <78136678+fictgenics@users.noreply.github.com>
2023-04-29 11:15:05 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,9 +1,6 @@
 import re
 import pandas as pd
 
-f = open('/Users/lakshya/Downloads/chat_analyzer/WhatsApp Chat with Shashank Tripathi.txt','r', encoding='utf-8')
-data = f.read()
-print(data)
 def preprocess(data):
     pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
@@ -12,7 +9,7 @@ def preprocess(data):
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
